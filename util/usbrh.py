1ac5cb78aa2f36f75b5e7198dffb5c5a96cf069d
tadokoro <hidekazu.tadokoro@gmail.com>
2018-06-20 11:17:58 +0900

    update usbrh.py

--- util/usbrh.py
+++ util/usbrh.py
@@ -17,7 +17,7 @@ def calc_temp(so_t):
     return d1 + d2 * so_t
 
 def calc_hum(so_rh, t_c):
-    rh_linear = c1 + c2 * so_rh + c3 * (so_rh * so_rh)
+    rh_linear = c1 + c2 * so_rh + c3 * so_rh * so_rh
     return (t_c - 25) * (t1 + t2 * so_rh) + rh_linear
 
 def usbrh_get_rawdata():
@@ -26,7 +26,7 @@ def usbrh_get_rawdata():
     try:
         data = [0]*7
         h.write(data)
-        out = h.read(7)
+        out = h.read(7, timeout_ms=50000)
     except:
         raise
     finally:
@@ -46,4 +46,4 @@ def usbrh_trans_rawdata(out):
 if __name__ == '__main__':
     raw_data = usbrh_get_rawdata()
     actual_data = usbrh_trans_rawdata(raw_data)
-    print('%.4f %.4f' % actual_data)
+    print('%.2f %.2f' % actual_data)
